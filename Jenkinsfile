acf3788db9734a630a81dabdb0bee781610b377c
Céline YE <celine.ye96@gmail.com>
2022-02-09 18:04:02 +0100

    Added tests

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,6 +2,12 @@ pipeline {
     agent any
 
     stages {
+        stage('docker compose build') {
+            steps {
+                bat 'docker compose build'
+            }
+        }        
+        
         stage('docker compose up') {
             steps {
                 bat 'docker compose up -d'
--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,29 @@
 const express = require('express')
-const session = require('express-session')
 const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.use(cors({ credentials: true, origin: 'localhost:3210' }))
+app.use(cors({ credentials: true, origin: true }))
 
-app.use(
-  session({
-    secret: 'secret string',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {},
-  })
-)
+let pageVisits = 0
 
 app.get('/', (req, res) => {
-  if (!req.session.pageCount) {
-    req.session.pageCount = 0
-  }
-
-  req.session.pageCount += 1
-
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3210')
-
-  console.log('Returning', {
-    pageCount: req.session.pageCount
-  })
+  pageVisits += 1
 
   res.send({
-    pageCount: req.session.pageCount
+    pageCount: pageVisits
+  })
+})
+
+app.post('/reset', (req, res) => {
+  pageVisits = 0
+  res.send({
+    success: pageVisits === 0
   })
 })
 
 app.listen(port, () => {
   console.log(`API listening on port ${port}`)
 })
+
+module.exports = { app }
